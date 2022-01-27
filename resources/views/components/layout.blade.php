5ea795f76ea7db117006e26baeb8ce02eb9c02a3
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-27 16:32:18 +0100

    Design the home page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 
     <!-- Scripts -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
@@ -17,6 +18,16 @@
     <title>{{ $title }}</title>
 </head>
 <body>
+    <nav class="py-16 px-20 w-full absolute flex items-center justify-between">
+        <a href="{{ route('home') }}" class="text-lg font-semibold text-gray-900">
+            Math Solver<span class="text-indigo-500">.</span>
+        </a>
+
+        <ul>
+            <li><a class="text-gray-800" href="{{ route('home') }}">Home</a></li>
+        </ul>
+    </nav>
+
     {{ $slot }}
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,34 @@
 <x-layout title="Math Solver">
-    <div class="p-8">
-        <h1 class="text-4xl font-bold mb-4">Home</h1>
+    <div class="h-screen flex items-center justify-center">
+        <div class="w-full">
+            <h1 class="text-5xl mb-8 font-bold text-center text-gray-900">Simplify expressions<span class="text-indigo-500">.</span></h1>
 
-        <form action="{{ route('simplify') }}" method="get">
-            <label for="expression">Simplify an expression:</label><br>
-            <input type="text" name="expression" id="expression" class="border border-gray-300" /><br>
-            <button type="submit">Simplify</button>
-        </form>
+            <p class="mb-16 text-lg text-gray-600 text-center">
+                <span class="font-semibold text-gray-800">Math Solver</span> is a tool
+                designed to help <span class="font-semibold text-gray-800">you</span> with math.
+            </p>
+
+            <div class="relative flex items-center max-w-xl mx-auto">
+                <form action="{{ route('simplify') }}" method="get" class="w-full">
+                    <input
+                        type="text" name="expression" id="expression"
+                        placeholder="Type your expression..."
+                        autofocus
+                        class="w-full px-6 py-4 placeholder-gray-500 bg-indigo-100 rounded-xl focus:outline-none"
+                        autocomplete="off"
+                        x-data="{ expression: '' }"
+                        x-model="expression"
+                        :class="{ 'font-mono': expression, 'tracking-wide': !expression }"
+                    />
+
+                    <button
+                        type="submit"
+                        class="absolute top-0 right-0 px-10 py-4 font-medium text-white transition bg-indigo-500 shadow-xl  focus:outline-none focus:bg-indigo-600 rounded-xl hover:bg-indigo-600 hover:shadow-lg"
+                    >
+                        Simplify
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 </x-layout>
