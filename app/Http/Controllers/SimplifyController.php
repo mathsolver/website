f7eda6ec598b0c883fddd82a5836005f3822e6ad
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-01-27 17:50:44 +0100

    Add an error message

--- app/Http/Controllers/SimplifyController.php
+++ app/Http/Controllers/SimplifyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use MathSolver\Math;
+use Throwable;
 
 class SimplifyController
 {
@@ -14,10 +15,21 @@ class SimplifyController
             return redirect()->route('home');
         }
 
+        try {
+            $expression = Math::from(request('expression'))->mathjax();
+            $simplified = Math::from(request('expression'))->simplify()->mathjax();
+        } catch (Throwable $exception) {
+            return view('simplify', [
+                'raw' => request('expression'),
+                'expression' => request('expression'),
+                'simplified' => 'error',
+            ]);
+        }
+
         return view('simplify', [
             'raw' => request('expression'),
-            'expression' => Math::from(request('expression'))->mathjax(),
-            'simplified' => Math::from(request('expression'))->simplify()->mathjax(),
+            'expression' => $expression,
+            'simplified' => $simplified,
         ]);
     }
 }
--- resources/views/simplify.blade.php
+++ resources/views/simplify.blade.php
@@ -6,8 +6,13 @@
             <h1 class="text-3xl font-bold text-gray-900">Simplify \( {{ $expression }} \) <span class="text-indigo-500">.</span></h1>
 
             <div class="bg-white p-8 mx-auto shadow-md rounded-lg">
-                <p class="font-semibold mb-4 text-indigo-700">Simplified:</p>
-                <p class="text-2xl">\( {{ $simplified }} \)</p>
+                @if($simplified === 'error')
+                    <p class="font-bold mb-4 text-red-600">Error:</p>
+                    <p class="font-medium text-2xl text-gray-800">An error has occured.</p>
+                @else
+                    <p class="font-bold mb-4 text-indigo-700">Simplified:</p>
+                    <p class="text-2xl">\( {{ $simplified }} \)</p>
+                @endif
             </div>
         </div>
     </div>
