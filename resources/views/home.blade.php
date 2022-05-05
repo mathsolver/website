75b9772085535bbb99483791a52f2f9abfb9d8fe
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-05-05 11:18:27 +0300

    Add examples

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,7 +57,7 @@
                     <div
                         x-cloak
                         x-show="showHelp"
-                        class="relative p-8 mb-12 -mt-4 bg-white rounded-lg shadow"
+                        class="relative px-8 pt-8 pb-6 mb-12 -mt-4 bg-white rounded-lg shadow"
                         x-transition:enter="transition ease-out duration-300"
                         x-transition:enter-start="opacity-0"
                         x-transition:enter-end="opacity-100"
@@ -83,8 +83,16 @@
                                 <td class="pb-2"><span class="font-mono">root(64, 3)</span> \( = \sqrt[3]{64} \)</td>
                             </tr>
                             <tr>
-                                <td class="pr-3">Fractions:</td>
-                                <td><span class="font-mono">frac(2, 3)</span> \( = \frac{2}{3} \)</td>
+                                <td class="pb-2 pr-3">Fractions:</td>
+                                <td class="pb-2"><span class="font-mono">frac(2, 3)</span> \( = \frac{2}{3} \)</td>
+                            </tr>
+                            <tr>
+                                <td class="pb-2 pr-3">Derivatives:</td>
+                                <td class="pb-2"><span class="font-mono">deriv(y^2, y)</span> \( = \frac{d}{dy}(y^2) \)</td>
+                            </tr>
+                            <tr>
+                                <td class="pb-2 pr-3">Logarithms:</td>
+                                <td class="pb-2"><span class="font-mono">log(8, 2)</span> \( = \log_2(8) \)</td>
                             </tr>
                         </table>
                     </div>
@@ -94,6 +102,9 @@
                         <a href="{{ route('simplify', ['expression' => '7x^2 + 5x * 4x']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( 7x^2 + 5x * 4x \)</a>
                         <a href="{{ route('simplify', ['expression' => 'root(18, 2) + root(32, 2)']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( \sqrt{18} + \sqrt{32} \)</a>
                         <a href="{{ route('simplify', ['expression' => '5 * frac(3, 9) + 4']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( 5 * \frac{3}{9} + 4 \)</a>
+                        <a href="{{ route('simplify', ['expression' => 'log(100, 10)']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( \log_{10}(100) \)</a>
+                        <a href="{{ route('simplify', ['expression' => 'deriv(x^2 + 3x)']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( \frac{d}{dx}(x^2 + 3x) \)</a>
+                        <a href="{{ route('simplify', ['expression' => 'x^frac(1,2) * x^frac(3, 2)']) }}" class="block px-6 py-3 text-center bg-gray-200 rounded-lg hover:bg-gray-300">\( x^{\frac{1}{2}} * x^{\frac{3}{2}} \)</a>
                     </div>
                 </form>
             </div>
