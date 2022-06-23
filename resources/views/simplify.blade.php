5f22c8450a4b9a80ba7378f1f9fc30a9914ca38b
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-06-23 13:25:03 +0200

    Remove dead code

--- resources/views/simplify.blade.php
+++ resources/views/simplify.blade.php
@@ -4,23 +4,16 @@
     <div class="w-full max-w-xl pt-56 pb-16 mx-auto">
         <h1 class="text-5xl font-bold text-gray-900">Simplify \( {{ $expression }} \)<span class="text-indigo-500">.</span></h1>
 
-        @if($simplified === 'error')
+        @forelse ($steps as $step)
             <div class="p-8 mx-auto mt-10 bg-white rounded-lg shadow-md">
-                <p class="mb-4 font-bold text-red-600">Error:</p>
-                <p class="text-2xl font-medium text-gray-800">An error has occured.</p>
+                <p class="mb-4 font-bold text-indigo-700">{{ $step['name'] }}</p>
+                <p>$$ {{ $step['result'] }} $$</p>
             </div>
-        @else
-            @forelse ($steps as $step)
-                <div class="p-8 mx-auto mt-10 bg-white rounded-lg shadow-md">
-                    <p class="mb-4 font-bold text-indigo-700">{{ $step['name'] }}</p>
-                    <p>$$ {{ $step['result'] }} $$</p>
-                </div>
-            @empty
-                <div class="p-8 mx-auto mt-10 bg-white rounded-lg shadow-md">
-                    <p class="mb-4 font-bold text-indigo-700">Simplified</p>
-                    <p>$$ {{ $simplified }} $$</p>
-                </div>
-            @endforelse
-        @endif
+        @empty
+            <div class="p-8 mx-auto mt-10 bg-white rounded-lg shadow-md">
+                <p class="mb-4 font-bold text-indigo-700">Simplified</p>
+                <p>$$ {{ $simplified }} $$</p>
+            </div>
+        @endforelse
     </div>
 </x-layout>
